0582ec6716bd641c99074e6404bb56208d6068ab
Mohishaik1 <shaikmohddinsharieff@gmail.com>
2025-11-12 11:14:42 +0000

    new

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from 'cors';
 // import bcrypt from 'bcrypt';
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
 //db
 import './utils/dbConnect.js';
@@ -10,7 +13,8 @@ import userRouter from './controllers/UsersControllers.js';
 import routing from "./controllers/contactController.js";
 import router from "./controllers/PackageControllers.js";
 
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = 5500;
 const app = express();
@@ -21,6 +25,14 @@ app.use(cors());
 app.use('/api/user', userRouter);
 app.use('/api/user', routing);
 app.use('/api/user', router);
+
+const buildPath = path.join(__dirname, "build");
+if (fs.existsSync(buildPath)) {
+  app.use(express.static(buildPath));
+  app.get(/^\/(?!api).*/, (req, res) => {
+    res.sendFile(path.join(buildPath, "index.html"));
+  });
+}
 
 
 app.get('/', (req,res)=>{
