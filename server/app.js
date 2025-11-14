1ba71a20ca5318108162d0e0b6f8acdc8c142a27
Mohishaik1 <shaikmohddinsharieff@gmail.com>
2025-11-14 15:27:37 +0000

    changes

--- server/app.js
+++ server/app.js
@@ -25,9 +25,7 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }));
 const corsOptions = {
   origin: [
     'https://www.mohiddinsharieff.xyz',
-    'http://localhost:5500',
-    'http://localhost:3000',
-    'http://127.0.0.1:5500'
+    'http://localhost:5500'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
